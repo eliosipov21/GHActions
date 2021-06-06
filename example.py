0a5d878a31061b956730a46edd78704d7382c0e1
eliosipov21 <eli.osipov2@gmail.com>
2021-06-06 10:58:29 -0700

    added subtract, multiply, divide method and tests for them

--- example.py
+++ example.py
@@ -1,2 +1,11 @@
 def addition(a, b):
     return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+import example 
+
+
+class TestCase(unittest.TestCase):
+
+    def test_add_1(self):
+        self.assertEqual(example.add(1,2), 3)
+
+    def test_subtract_1(self):
+        self.assertEqual(example.subtract(2,1), 1)
+
+    def test_multiply_1(self):
+        self.assertEqual(example.multiply(2,1), 2)
+
+    def test_divide_1(self):
+        self.assertEqual(example.divide(2,1), 2)
+
+if __name__ == '__main__':
+    unittest.main()
